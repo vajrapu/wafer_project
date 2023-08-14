5c74e0b72bb4a5fa447da3c2995811325f7b77e9
Girija Shankar Vajrapu <vajrapushankar@pop-os.localdomain>
2023-08-14 08:24:49 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It is a Wafer project using MLOps',
+    author='Girija Shankar Vajrapu',
+    license='MIT',
+)
